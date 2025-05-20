3e439d3acc437211dcfeddaefb1e822f2f00c216
Lenfantsauvage998 <144186816+Lenfantsauvage998@users.noreply.github.com>
2025-05-20 01:03:57 -0500

    feat: servicio CRUD para Edge

---
+++ Back/src/services/edgeService.js
@@ -0,0 +1,20 @@
+import Edge from '../models/Edge.js';
+
+// Listar todas las aristas 
+export const fecthAllEdges = () => 
+    Edge.find().populate('from to').lean();
+
+// Crear arista 
+export const createEdga = ({from,to,weight}) => {
+    const edge = new Edge({from,to,weight})
+    return edge.save();
+}
+
+// Actualizar peso 
+
+export const updateEdge = (id,data) => 
+    Edge.findByIdAndUpdate(id, data, { new: true, runValidators: true });
+
+// Eliminar arista
+export const deleteEdge = (id) =>
+  Edge.findByIdAndDelete(id);
